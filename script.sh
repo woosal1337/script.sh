ad911f8ea58899d3559a5b26b3dcbd56a81f4d08
Vusal Ismayilov <vusal.ismayilov@std.medipol.edu.tr>
2021-03-12 16:26:44 +0300

    1.0

---
+++ script.sh
@@ -0,0 +1,23 @@
+# Stacer
+sudo add-apt-repository ppa:oguzhaninan/stacer
+sudo apt-get update
+sudo apt-get install stacer
+
+# Docker
+sudo snap install docker
+
+# Google Cloud SDK
+sudo snap install google-cloud-sdk --classic
+
+# FFmpeg
+sudo snap install ffmpeg
+
+# Handbrake
+sudo snap install handbrake-jz
+
+# Termius - SSH Client
+sudo snap install termius-app
+
+# System Monitor Tool
+sudo snap install gnome-system-monitor
+
